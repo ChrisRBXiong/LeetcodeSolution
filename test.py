272a998bcff690e6ae82328a98ec4f2959c84944
chris <chrisrbxiong@gmail.com>
2020-05-18 09:21:03 +0800

    2020.05.18

--- test.py
+++ test.py
@@ -1,40 +1,57 @@
+# # class Solution:
+# #     def solve(self , n , m , a ):
+# #         l, r = 1, min(a)
+# #         while l < r:
+# #             mid = l + (r-l) // 2
+# #             cnt = self.helper(a, mid)
+# #             if cnt >= m:
+# #                 l = mid + 1
+# #             else:
+# #                 r = mid 
+# #         # return res
+# #         cnt = self.helper(a, l)
+# #         if cnt < m:
+# #            return l - 1
+# #         else:
+# #            return l
+
+# #     def helper(self, a, min_count):
+# #         array = a 
+# #         cnt = 0
+# #         for a in array:
+# #             cnt += a // min_count
+# #         return cnt 
+
+
+# # Solution().solve(3,5,[3,5,6])
+
+
+
+
+
 # class Solution:
-#     def solve(self , n , m , a ):
-#         l, r = 1, min(a)
-#         while l < r:
-#             mid = l + (r-l) // 2
-#             cnt = self.helper(a, mid)
-#             if cnt >= m:
-#                 l = mid + 1
-#             else:
-#                 r = mid 
-#         # return res
-#         cnt = self.helper(a, l)
-#         if cnt < m:
-#            return l - 1
-#         else:
-#            return l
-
-#     def helper(self, a, min_count):
-#         array = a 
-#         cnt = 0
-#         for a in array:
-#             cnt += a // min_count
-#         return cnt 
+#     def function(self, arrays, n, d):
+#         if n == 0:
+#             return 0
+#         arrays = sorted(arrays)
+#         cur = arrays[0]
+#         count = 1
+#         for n in arrays:
+#             if n - cur >= d:
+#                 cur = n 
+#                 count += 1
+#         return count 
 
 
-# Solution().solve(3,5,[3,5,6])
+# print(Solution().function([1,4,2,8,5,7],6,2))
 
-
-
-def function(k):
-    s, not_s = 1, 0
-    for i in range(k):
-        prev_s, prev_not_s = s, not_s
-        not_s = prev_s + 2*prev_not_s
-        s = 3*prev_not_s
-        s, not_s = prev_s, prev_not_s
-    return s 
+        
+import numpy as np
+a = np.array([83,94,77,79,80,90,90,96,95,87,92,96,83,85])
+b = np.array([1,1,3,2,2,1,1,1,3,3,2,2,3,2])
+assert len(a) == len(b)
+c = np.sum(a * b)
+print(c/sum(b))
 
 
 
